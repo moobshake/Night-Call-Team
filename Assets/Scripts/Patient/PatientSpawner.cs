db4f2ea9e0bf77958ba9a83b3599f57c6d73155f
Yu Hui <yuhui@Yus-MacBook-Air.local>
2022-08-05 18:00:31 +0800

    Spawns patient on bed on game start

--- Assets/Scripts/Patient/PatientSpawner.cs
+++ Assets/Scripts/Patient/PatientSpawner.cs
@@ -15,6 +15,11 @@ public class PatientSpawner : MonoBehaviour
     public TextAsset text;
     public GameObject patient;
     public GameObject[] patients;
+    private Quaternion sleepPos;
+
+    // Location
+    public GameObject[] beds;
+    public bool [] occupied;
 
     // Helps with assigning patient info to prefab
     private Dictionary<string, string> details;
@@ -23,18 +28,41 @@ public class PatientSpawner : MonoBehaviour
 
     void Start()
     {   
+        beds = GameObject.FindGameObjectsWithTag("Bed");
+        occupied = new bool[beds.Length];
+        sleepPos = Quaternion.Euler(-90,0,0);
         CreatePatient();
     }
 
     void CreatePatient(){
         PopulateDetails();
-        Instantiate(patient);
+        Instantiate(patient, GetRoom(), sleepPos);
         PatientInfo patientInfo = (PatientInfo)patient.GetComponent("PatientInfo");
         patientInfo.Name = details["Name"];
         patientInfo.Age = details["Age"];
         patientInfo.Race = details["Race"];
         patientInfo.Gender = details["Gender"];
         patientInfo.Condition = details["Condition"];
+    }
+
+    Vector3 GetRoom(){
+        int i = 0;
+
+        foreach (GameObject bed in beds)
+        {   
+            if(!occupied[i]){
+                Vector3 offset = new Vector3(0.0f, 0.7f, 0.0f);
+                Vector3 bedpos = bed.transform.position + offset;
+                occupied[i] = true;
+                return bedpos;
+            }
+            i++;
+
+            if(i == beds.Length){
+                break;
+            } 
+        }
+        return new Vector3(-10f, -10f, -10f);
     }
 
     void PopulateDetails(){
