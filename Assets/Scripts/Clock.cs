4613df4e451102b3035fb054e69daeadd28ba0b6
Darryl Tan <darryl@Darryls-MacBook-Pro.local>
2022-08-10 22:23:34 +0800

    Change clock to a timer instead

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -10,33 +10,51 @@ public class Clock : MonoBehaviour
     private TextMeshProUGUI textClock;
     public int secondsToMinusHealth;
     public bool minusHealth;
-    private float current;
+    public float timeRemaining;
+    public bool timerIsRunning = false;
 
-    void Awake()
+    private int originalTimeS;
+    private int originalTimeM;
+    private float previousMinus;
+
+    void Start()
     {
         textClock = GetComponent<TextMeshProUGUI>();
-        current = Time.time;
         minusHealth = false;
+        timerIsRunning = true;
+        originalTimeM = Mathf.FloorToInt(timeRemaining / 60);
+        originalTimeS = Mathf.FloorToInt(timeRemaining % 60);
+        previousMinus = timeRemaining;
     }
 
     // Update is called once per frame
     void Update()
     {
-        DateTime time = DateTime.Now;
-        string hour = LeadingZero(time.Hour);
-        string minute = LeadingZero(time.Minute);
-        string second = LeadingZero(time.Second);
-        textClock.text = hour + ":" + minute + ":" + second;
-
-        if (Time.time - current >= secondsToMinusHealth && !minusHealth)
+        if (timerIsRunning)
         {
-            minusHealth = true;
-            current = Time.time;
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+                if (Mathf.FloorToInt((previousMinus - timeRemaining) % 60) >= secondsToMinusHealth && !minusHealth) {
+                    minusHealth = true;
+                    previousMinus = timeRemaining;
+                }
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
         }
     }
 
-    string LeadingZero(int n)
+    void DisplayTime(float timeToDisplay)
     {
-        return n.ToString().PadLeft(2, '0');
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        textClock.text = string.Format("{0:00}:{1:00} / {2:00}:{3:00}", minutes, seconds, originalTimeM, originalTimeS);
     }
 }
